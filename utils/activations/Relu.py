bdb72d4007125dea8a327b201bb81c4a0cabe5cb
Nguyễn Anh Kiệt <anhkietn711@gmail.com>
2026-07-05 21:42:25 +0700

    Create Relu.py

---
+++ utils/activations/Relu.py
@@ -0,0 +1,7 @@
+##Activation Functions
+class Relu:
+    def __call__(self, x):
+        self.last_input = x
+        return np.maximum(0, x)
+    def backward(self, grad_output):
+        return grad_output * (self.last_input > 0)
